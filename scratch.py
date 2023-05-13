dc366075843657ebc883cec6b3b2a2f43cb2e366
MalMill <133486750+MalMill@users.noreply.github.com>
2023-05-13 13:45:14 +0200

    add tasks solutions

--- scratch.py
+++ scratch.py
@@ -7,17 +7,32 @@ Np. dla stringa 'ala ma kota' wynikiem powinno być a, 4
 funkcji jest 'spłaszczenie listy' i podanie wyniku, czyli, jeśli funkcja przyjmie listę [1, 2, [3, [4, 5]], 6, [[[7]]], 8, 9, 10]
 to wynik powinien wynosić '[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', liczba zagnieżdżeń w liście może być nieskończona, nie możemy zakładać
 konkretnej liczby.
+
+Po zrobieniu zadania, pushujemy wynik na nowy branch który stworzyliśmy i tworzymy pull requesta, pull request możemy zmerge;ować tylko
+po akceptacji trenera
 """
 
-def count_most_common_sign(input_str):
-    sign_count = {}
-    for sign in input_str:
-        if sign in signs_count:
-            sign_count[sign] += 1
-        else:
-            sign_count[sign] = 1
-    most_common_sign = max(sign_count, key=sign_count.get)
-    return most_common_sign, sign_count[most_common_sign]
+# Zadanie 1
+def popular_sign(string):
+    counter = {}
+    for sign in string:
+@@ -25,4 +27,19 @@ def popular_sign(string):
+    return sorted_counter[0]
 
-result = count_most_common_sign('ala ma kota')
-print(result)
+string = "ala ma kota"
+print(popular_sign(string))
+print(popular_sign(string))
+
+
+# Zadanie 2
+def flatten_list(lst):
+    result = []
+    for element in lst:
+        if isinstance(element, list):
+            result.extend(flatten_list(element))
+        else:
+            result.append(element)
+    return result
+
+lst = [1, 2, [3, [4, 5]], 6, [[[7]]], 8, 9, 10]
+print(flatten_list(lst))
